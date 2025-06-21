c7fcb3dd524dca8686cf76145b0e302b0e908922
Tash Broadwater <tbroadwater87@users.noreply.github.com>
2025-06-20 19:00:23 -0600

    Remove deprecated @astrojs/image integration - fix build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image   from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://thebytelite.com",
   integrations: [
     tailwind(),
-    image(),
     sitemap()
   ]
 });
